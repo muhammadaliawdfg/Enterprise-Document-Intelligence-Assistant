4ec94630e864f88014b6891be877dc839d33cd41
muhammadaliawdfg <muhammadali35484@gmail.com>
2026-01-27 09:50:50 +0500

    module 2 completed

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,7 +1,25 @@
-from fastapi import APIRouter
-
+import os
+from fastapi import APIRouter, UploadFile,File,HTTPException
+import shutil
 router = APIRouter()
-
+UPLOAD_DIR = "storage/documents"
 @router.get("/health")
 def health_check():
     return {"status": "EDIA backend is running"}
+@router.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    # check file type
+    if not file.filename.endswith(".pdf"):
+        raise HTTPException(status_code=400, detail="Only PDF files are allowed")
+    # create storage directory if not exists
+    
+    os.makedirs(UPLOAD_DIR, exist_ok=True)
+    # save file to storage directory
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as f:
+        shutil.copyfileobj(file.file, f)
+    return {
+        "message": "File uploaded successfully",
+        "filename": file.filename
+        }      
